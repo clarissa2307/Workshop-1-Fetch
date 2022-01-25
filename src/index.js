702e866868e43439937b9a5b0f1c2497b613d6d5
clarissa2307 <marthaleo07@gmail.com>
2022-01-25 14:52:18 -0600

    creado nodos

---
+++ src/index.js
@@ -0,0 +1,36 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+const url = "https://platzi-avo.vercel.app/api/avo"; 
+
+//web  api
+//conectarnos al servidor
+window.fetch(url)
+//procesar la respuesta y convertirla en json
+.then(respuesta => respuesta.json())
+
+//json -> Data -> renderizar info
+.then((responseJson) => {
+    const todosLosItems = [];
+    responseJson.data.forEach((item) => {
+        //crear imagen
+        const imagen = document.createElement("img");
+        
+        //crear titulo
+        const title = document.createElement("h2");
+        
+        //crear precio
+        const price = document.createElement("div");
+        
+
+        const container = document.createElement("div");
+        container.append(imagen, title, price);
+
+        todosLosItems.push(container);
+ 
+    });
+    document.body.append(...todosLosItems);
+});
+
