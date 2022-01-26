ee8df2f4db8f1cdabc6ee9f727d5aa3507c54650
clarissa2307 <marthaleo07@gmail.com>
2022-01-25 20:27:02 -0600

    Usando la Api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@
  **/
  const baseUrl = "https://platzi-avo.vercel.app";
  const appNode = document.querySelector('#app')
+ const formatPrice = (price) => {
+   const newPrice = new window.Intl.NumberFormat('en-En', {
+        style: "currency",
+        currency: "USD",
+     }).format(price)
+     
+     return newPrice;
+ };
 
 
 //web  api
@@ -18,16 +26,37 @@ window
     const todosLosItems = [];
     responseJson.data.forEach((item) => {
         //crear imagen
-        const imagen = document.createElement("img");
-        imagen.src = `${baseUrl}${item.image}`
+        const imagen = document.createElement('img');
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+        imagen.src = `${baseUrl}${item.image}`;
+        //craer titulo
+         const title = document.createElement('h2');
+         title.className = "text-lg";
+         title.textContent = item.name;
+         
+         //crear precio
+         const price = document.createElement("div");
+         price.className = "text-gray-600";
+         price.textContent = formatPrice(item.price);
+         
+          //unir price y title
+          const priceAndTitle = document.createElement('div');
+          priceAndTitle.className = "text-center md:text-left";
+          priceAndTitle.appendChild(title);
+          priceAndTitle.appendChild(price);
+
+          //unir imagen con priceAndTitle 
+          const card = document.createElement('div');
+          card.className = "md:flex bg-white rounded-lg p-6 hover:big-gray-300";
+          card.appendChild(imagen);
+          card.appendChild(priceAndTitle);
+
+         
+         
         
-        //crear titulo
-        const title = document.createElement("h2");
-        title.textContent = item.name;
+
         
-        //crear precio
-        const price = document.createElement("div");
-        price.textContent = item.price;
+       
         
 
         const container = document.createElement("div");
